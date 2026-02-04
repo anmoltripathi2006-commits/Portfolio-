f678246fed3f58f2ccc175d3e4efbdd931cc0b5a
anmoltripathi2006-commits <anmoltripathi2006@gmail.com>
2026-02-04 21:41:40 +0530

    Refactor emailjs initialization and form submission

--- script.js
+++ script.js
@@ -1,34 +1,19 @@
-// EMAILJS
-emailjs.init("PASTE_YOUR_PUBLIC_KEY_HERE");
+// EMAILJS INIT
+(function () {
+  emailjs.init("YOUR_PUBLIC_KEY"); // replace later
+})();
 
-document.getElementById("contactForm").addEventListener("submit", function(e) {
+document.getElementById("contact-form").addEventListener("submit", function(e){
   e.preventDefault();
 
   emailjs.sendForm(
-    "service_xgul5s7",
-    "template_whk7z0s",
+    "YOUR_SERVICE_ID",
+    "YOUR_TEMPLATE_ID",
     this
   ).then(
-    () => alert("Message sent successfully!"),
+    () => alert("Message Sent Successfully!"),
     () => alert("Failed to send message")
   );
 
   this.reset();
-});
-
-// SCROLL ANIMATION
-window.addEventListener("scroll", () => {
-  document.querySelectorAll(".reveal").forEach(el => {
-    if (el.getBoundingClientRect().top < window.innerHeight - 100) {
-      el.classList.add("active");
-    }
-  });
-});
-
-// DARK / LIGHT MODE
-const toggleBtn = document.getElementById("themeToggle");
-toggleBtn.addEventListener("click", () => {
-  document.body.classList.toggle("light");
-  toggleBtn.textContent =
-    document.body.classList.contains("light") ? "☀️" : "🌙";
 });
